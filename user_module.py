39144452572a0a7f2df24908788cce13e75fdf83
Ace <edisonford2006@gmail.com>
2025-05-13 22:47:14 +0100

    new user and referral module

---
+++ user_module.py
@@ -0,0 +1,9 @@
+
+class user:
+    def __init__(self, id, departmentid, department, full_name, password, email):
+        self.user_id =  id
+        self.deptid = departmentid
+        self.department = department
+        self.name = full_name
+        self.pswrd = password
+        self.email = email
